3eb8833f309ec6169128176bde70e07e7d459f71
DM <bezrukovf@gmail.com>
2022-08-02 22:25:08 +0400

    add todo list axios

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
 const btn = document.querySelector(`.button`)
 const btnDelAll = document.querySelector(`.remove-all`)
+const btnGetTask = document.querySelector(`.btn-get-task`)
 const div = document.querySelector(`.image`)
 const inp = document.querySelector(`.input`)
+const ulList = document.querySelector(`.list`)
 
-// btnDelAll.addEventListener(`click`, delALL)
+btnDelAll.addEventListener(`click`, delALLImage)
+btn.addEventListener(`click`, init)
+btnGetTask.addEventListener(`click`, startTaskResponse)
 
-function delALLImage() {
+function delALLImage(e) {
 	e.preventDefault()
 	div.innerHTML = ``
+
 }
-
-btn.addEventListener(`click`, getImage)
-
-function getImage(e) {
+function init(e) {
 	e.preventDefault()
 	if (inp.value > 0) {
-		getStart()
+		startImageResponse()
 	}
-
 }
-
-function getStart() {
-	const promise = getResponse(inp.value)
+function startImageResponse() {
+	const promise = getImage(inp.value)
 	promise.then((data) => {
 		showImage(data)
 	})
+}
+
+
+function startTaskResponse(e) {
+	e.preventDefault()
+	createGoal(inp.value).then(() => {
+		const promise = getTask()
+		promise.then((data) => {
+			showTask(data)
+		}).then(() => {
+			completeTask()
+		})
+	})
+	// setTimeout(() => {
+
+	// }, 3000)
 
 }
 
@@ -34,4 +50,44 @@ function showImage(data) {
 		img.src = el.url
 		div.append(img)
 	});
+}
+
+
+function showTask(data) {
+	ulList.innerHTML = ``
+	data.map(task => {
+		ulList.insertAdjacentHTML(
+			'afterbegin', `<li data-id="${task.id}" class="list__item">
+			<input class="list__input" type="checkbox" name="" id="${task.id}">
+			<label class="list__label" for="${task.id}">${task.title}</label>
+			<button disabled type="button" class="list__button">Удалить</button>
+		</li>`
+		)
+	});
+}
+
+function completeTask() {
+	const listInput = document.getElementsByClassName(`list__input`)
+	const arr = [...listInput]
+	arr.forEach((el) => {
+		el.addEventListener(`click`, () => {
+			if (el.checked) {
+				el.nextElementSibling.style.textDecoration = `line-through`
+				el.nextElementSibling.nextElementSibling.disabled = false
+				removeTask(el)
+			} else {
+				el.nextElementSibling.style.textDecoration = ``
+				el.nextElementSibling.nextElementSibling.disabled = true
+			}
+		})
+	})
+}
+function removeTask(el) {
+	if (!!el) {
+		el.nextElementSibling.nextElementSibling.addEventListener(`click`, () => {
+			console.log(el.parentElement.dataset.id);
+			deleteTask(el.parentElement.dataset.id)
+			el.parentElement.remove()
+		})
+	}
 }
--- service.js
+++ service.js
@@ -1,5 +1,43 @@
-function getResponse(inp) {
+function getImage(inp) {
 	const promise = axios.get(`https://jsonplaceholder.typicode.com/photos?id=${inp}`)
+	return promise
+		.then((response) => {
+			return response.data
+		})
+}
+function updateGoal(title, id, done) {
+	const promise = axios.put(`https://repetitora.net/api/JS/Tasks`, {
+		widgetid: 334334,
+		title: title,
+		taskid: id,
+		done: done
+	})
+	return promise
+		.then((response) => {
+			return response.data
+		})
+}
+function getTask() {
+	const promise = axios.get(`https://repetitora.net/api/JS/Tasks?widgetid=334334&count=30`)
+	return promise
+		.then((response) => {
+			return response.data
+		})
+}
+function deleteTask(id) {
+	const promise = axios.delete(`https://repetitora.net/api/JS/Tasks?widgetid=334334&taskId=${id}`)
+	return promise
+		.then((response) => {
+			return response.data
+		})
+}
+
+
+function createGoal(title) {
+	const promise = axios.post(`https://repetitora.net/api/JS/Tasks`, {
+		widgetid: 334334,
+		title: title
+	})
 	return promise
 		.then((response) => {
 			return response.data
